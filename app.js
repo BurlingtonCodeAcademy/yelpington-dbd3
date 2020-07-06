8d8056fa6f74873af9085d3581a6bb306b28c418
dbd3 <63477877+dbd3@users.noreply.github.com>
2020-07-05 22:33:03 -0400

    First commit

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 const express = require('express')
+const app = express()
+const port = process.env.PORT || 5050
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {     res.send(filename, path.resolve('/public/index.html')) })
+
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
